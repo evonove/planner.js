84be82964282845581fe2673791c960752d7d841
Emanuele Palazzetti <emanuele.palazzetti@evonove.it>
2013-11-08 12:53:25 +0100

    Column partial on body template

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 ;(function($, Planner) { 'use strict';
 
-    var _htmlNode = $('.container');
-    var _htmlPlanner = Planner.Templates.body({});
+    var _htmlNode = $('.container'),
+        _dayColumn = [
+            {name: 'Monday'},
+            {name: 'Tuesday'},
+            {name: 'Wednesday'},
+            {name: 'Thursday'},
+            {name: 'Friday'},
+            {name: 'Saturday'},
+            {name: 'Sunday'}
+        ];
 
+    var _htmlPlanner = Planner.Templates.body({columns: _dayColumn});
     _htmlNode.html(_htmlPlanner);
 })(jQuery, Planner);
