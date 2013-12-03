611de494effb46dc6c5b3637c82838f8faeb1a92
Emanuele Palazzetti <emanuele.palazzetti@evonove.it>
2013-12-03 12:06:49 +0100

    Card object automatically generate DOM elements during init

--- src/js/models/Card.js
+++ src/js/models/Card.js
@@ -12,11 +12,36 @@
         this.start = start;
         this.end = end;
         this.assignees = assignees;
+
+        this.$element = this.generateDOM();
+    };
+
+    // Card class definition with prototype methods
+    // --------------------------------------------
+
+    Card.prototype.generateDOM = function() {
+        var self = this;
+        var generatedElements = [];
+
+        // Generate a standard DOM element
+        var cardDOM = $(Planner.Templates.card(self));
+        var element;
+
+        // Generate DOM element for each assignees (column)
+        this.assignees.forEach(function(value) {
+            element = cardDOM.clone();
+            element.data('column', value);
+
+            generatedElements.push(element);
+        });
+
+        return generatedElements;
     };
 
     // Methods that should be overridden by your
     // application to enable persistence
     // -----------------------------------------
+
     Card.prototype.save = function() {};
     Card.prototype.delete = function() {};
 
