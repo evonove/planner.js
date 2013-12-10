1521f716ee7a8f368600c10ef093f99b7e76d589
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-10 17:51:13 +0100

    Added utils for leading zeros on string

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Planner = Planner || {};
+Planner.Utils = Planner.Utils || {};
 Planner.Helpers = Planner.Helpers || {};
 Planner.Templates = Planner.Templates || {};
 Planner.Model = Planner.Model || {};
---
+++ src/js/utils.js
@@ -0,0 +1,15 @@
+;(function($, Planner) { 'use strict';
+
+    // Javascript utilities
+    // --------------------
+
+    Planner.Utils = {
+        pad: function(text, slice) {
+            if (typeof slice === 'undefined') {
+                slice = -2;
+            }
+            return ('0' + text).slice(slice);
+        }
+    };
+
+})(jQuery, Planner);
