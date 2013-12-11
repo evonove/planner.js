00d9efe20cb941c0175ff2316fcc28e3257f2078
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-11 17:52:53 +0100

    Bootstrap CRUD plugin

--- src/js/plugins/crud.js
+++ src/js/plugins/crud.js
@@ -1,3 +1,51 @@
-;(function() { 'use strict';
+;(function($, Plugins) { 'use strict';
 
-})();
+    // Plugin constructor and defaults
+    // -------------------------------
+
+    var Crud = function(element, options) {
+        this.$element = $(element);
+        this.options = options;
+    };
+
+    Crud.DEFAULTS = {
+        readOnly: true
+    };
+
+    // Prototype functions
+    // -------------------
+
+    // Crud plugin definition
+    // ----------------------
+
+    var old = $.fn.crud;
+
+    $.fn.crud = function(option) {
+        return this.each(function() {
+            var $this = $(this);
+            var data = $this.data('pl.plugins.crud');
+            var options = $.extend({}, Crud.DEFAULTS, $this.data(), typeof option === 'object' && option);
+
+            // If this node isn't initialized, call the constructor
+            if (!data) {
+                $this.data('pl.plugins.crud', (data = new Crud(this, options)));
+            }
+        });
+    };
+
+    $.fn.crud.constructor = Crud;
+
+    // Crud no conflict
+    // ----------------
+
+    $.fn.crud.noConflict = function() {
+        $.fn.crud = old;
+        return this;
+    };
+
+    // Register this plugin to plugins list
+    // ------------------------------------
+
+    Plugins.register('crud', $.fn.crud);
+
+})(jQuery, Planner && Planner.Plugins);
