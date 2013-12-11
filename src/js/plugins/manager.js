ab2c5eac46d785c213242f2a92e86e40b9843eee
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2013-12-11 17:50:20 +0100

    Added a plugin registration and call manager

---
+++ src/js/plugins/manager.js
@@ -0,0 +1,23 @@
+;(function($, Planner) { 'use strict';
+
+    Planner.Plugins = Planner.Plugins || {};
+    Planner.Plugins.fn = [];
+
+    // Register plugins callback
+    // -------------------------
+
+    Planner.Plugins.register = function(pluginName, fn) {
+        Planner.Plugins.fn[pluginName] = fn;
+    };
+
+    Planner.Plugins.call = function(pluginName, context, params) {
+        var fn = Planner.Plugins.fn[pluginName];
+
+        if (typeof fn === 'function') {
+            fn.apply(context, params);
+        } else {
+            console.error('Chosen plugin is not available: ' + pluginName);
+        }
+    };
+
+})(jQuery, Planner);
