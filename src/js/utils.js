b86542dc04a04d3b148cf17f258b066e678d325b
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-01-20 13:02:10 +0100

    Better imports

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,13 +3,11 @@
     // Javascript utilities
     // --------------------
 
-    Planner.Utils = {
-        pad: function(text, slice) {
-            if (typeof slice === 'undefined') {
-                slice = -2;
-            }
-            return ('0' + text).slice(slice);
+    Planner.Utils.pad = function (text, slice) {
+        if (typeof slice === 'undefined') {
+            slice = -2;
         }
-    };
+        return ('0' + text).slice(slice);
+    }
 
 })(Planner);
