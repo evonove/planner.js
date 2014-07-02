abed1b8183db2d6e335ef34d369ec22e9d81811e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-07-02 17:18:05 +0200

    planner instance constructor related to #3

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -6,7 +6,3 @@ Planner.Events = Planner.Events || {};
 Planner.Helpers = Planner.Helpers || {};
 Planner.Templates = Planner.Templates || {};
 Planner.Models = Planner.Models || {};
-
-// Initialize HashMaps to store planner Card status
-Planner.mapCard = new HashMap();
-Planner.mapDom = new HashMap();
---
+++ src/js/instance.js
@@ -0,0 +1,15 @@
+;(function(Planner) { 'use strict';
+
+    // Defines Planner.js instance
+    // ---------------------------
+
+    Planner.Instance = function(element, options) {
+        this.element = element;
+        this.options = options;
+
+        // Initialize HashMaps to store planner Card status
+        this.mapCard = new HashMap();
+        this.mapDom = new HashMap();
+    };
+
+})(Planner);
