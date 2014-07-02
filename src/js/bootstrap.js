9fa4bde563bde6167855ac42b2d29a5f81cebe0e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-07-02 16:20:42 +0200

    extend for utils

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -3,7 +3,6 @@
 // Initialize namespaces
 var Planner = Planner || {};
 Planner.Events = Planner.Events || {};
-Planner.Utils = Planner.Utils || {};
 Planner.Helpers = Planner.Helpers || {};
 Planner.Templates = Planner.Templates || {};
 Planner.Models = Planner.Models || {};
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,13 +1,31 @@
-;(function(Planner) { 'use strict';
+;(function(Utils) { 'use strict';
 
     // Javascript utilities
     // --------------------
 
-    Planner.Utils.pad = function (text, slice) {
+    Utils.pad = function(text, slice) {
         if (typeof slice === 'undefined') {
             slice = -2;
         }
         return ('0' + text).slice(slice);
     };
 
-})(Planner);
+    Utils.extend = function(out) {
+        out = out || {};
+
+        for (var i = 1; i < arguments.length; i++) {
+            if (!arguments[i]) {
+                continue;
+            }
+
+            for (var key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key)) {
+                    out[key] = arguments[i][key];
+                }
+            }
+        }
+
+        return out;
+    };
+
+})(Planner.Utils = Planner.Utils || {});
