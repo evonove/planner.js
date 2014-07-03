0a6d243e7e149e0da01b7f594547bb55bfbcc1c5
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-07-03 09:48:55 +0200

    creating a default instance of planner with data-api, related to #3

--- src/js/instance.js
+++ src/js/instance.js
@@ -10,6 +10,31 @@
         // Initialize HashMaps to store planner Card status
         this.mapCard = new HashMap();
         this.mapDom = new HashMap();
+
+        // Check if columns and rows are set otherwise use a default planner
+        if (options.columnLabels.length === 0) {
+            options.columnLabels = Planner.Helpers.getDefaultColumns();
+        }
+
+        if (options.rowLabels.length === 0) {
+            options.rowLabels = Planner.Helpers.getDefaultRows();
+        }
+
+        // Edit some properties
+        options.visibleColumns = options.visibleColumns > options.columnLabels.length ? options.columnLabels.length : options.visibleColumns;
+
+        // Initialize planner template only if required
+        // TODO: better implementation required
+        if (options.show) {
+            this.element.innerHTML = Planner.Templates.body(options);
+        }
+
+        // Append all computed CSS
+        var head = document.querySelector('head');
+        head.appendChild(Planner.Helpers.computedCSS());
+
+        // Load attached plugins
+        Planner.Plugins.load(options.plugins, this);
     };
 
 })(window.Planner = window.Planner || {});
--- src/js/planner.js
+++ src/js/planner.js
@@ -19,9 +19,9 @@
     };
 
     PlanningChart.DEFAULTS = {
-        // TODO: use a better strategy to block planner draw
+        // TODO: use a better strategy to avoid planner draw
         show: true,
-        model: Planner.Models.Card,
+        // model: Planner.Models.Card, TODO: disabled for now
         plugins: [],
         timeslots: 4,
         timeslotHeight: 25,
@@ -39,18 +39,18 @@
     document.addEventListener('DOMContentLoaded', function(){
         var plannerElements = document.querySelectorAll('[data-planner="container"]');
 
-        plannerElements.forEach(function(item, i) {
+        for (var i = 0; i < plannerElements.length; i++) {
             // Get all params from selectors
-            var plugins = item.getAttribute('data-plugins');
+            var plugins = plannerElements[i].getAttribute('data-plugins');
             plugins = (plugins && plugins.split(' ')) || [];
 
             // Load default plugins
             plugins.push('slider');
 
-            // TODO: Planner initialization
-            // $planner.data('plugins', plugins);
-            // $planner.planner($planner.data());
-        });
+            // Planner initialization
+            // TODO: how to get options from there?
+            new window.Planner.Instance(plannerElements[i], PlanningChart.DEFAULTS);
+        }
     });
 
     window.Planner = PlanningChart;
