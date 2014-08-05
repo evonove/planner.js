9ef8210df3cc35821570aa8fe3f0bd7a1dfe4916
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-08-05 16:38:34 +0200

    enhanced collision draft with a correct splitting rule

--- src/js/plugins/collision.js
+++ src/js/plugins/collision.js
@@ -1,77 +1,95 @@
-(function (Plugins, Utils, undefined) {
+(function (Plugins, Utils, Set, undefined) {
   'use strict';
 
   // Plugin constructor and defaults
   // -------------------------------
 
   var Collision = function (element, options, planner) {
+    // TODO: use these as key for a type -> [] structure where type is a rule and [] is an array of object to order
     var SPLITTING = 0
       , OLDEST_ON_LEFT = 1;
 
-    function _detectCollisions (card, dom) {
-      // Checking range
-      var min = parseInt(dom.getAttribute('data-start'), 10)
-        , max = parseInt(dom.getAttribute('data-end'), 10)
-        , collisionGroup = []
-        , splitGroup = []
-        , currentMin
-        , currentMax;
-
+    function _manager (card, dom) {
       // Get all column cards except this one
-      var doms = dom.parentElement.parentElement.querySelectorAll('.planner-card:not([data-id="' + card.id + '"])');
-      for (var i = 0; i < doms.length; i++) {
-        currentMin = parseInt(doms[i].getAttribute('data-start'), 10);
-        currentMax = parseInt(doms[i].getAttribute('data-end'), 10);
-
-        // Collision ?
-        var collision = min >= currentMin && min <= currentMax || max >= currentMin && max <= currentMax;
-        collision = collision ? true : currentMin >= min && currentMax <= max || currentMin >= min && currentMax <= max;
-        var rule = collision && _precedenceRules(min, currentMin);
-
-        switch (rule) {
-          case SPLITTING:
-            splitGroup.push(doms[i]);
-            break;
-          case OLDEST_ON_LEFT:
-            collisionGroup.push(doms[i]);
-            break;
-        }
-      }
-
-      // TODO: in theory, this conflict is a collision OR a split
-      (collisionGroup.length > 0  || splitGroup.length > 0) && _resolveCollisions(dom, collisionGroup, splitGroup);
-    }
-
-    function _resolveCollisions (dom, collisionGroup, splitGroup) {
-
-      var startingPosition = Planner.Utils.offset(dom.parentNode).left;
-      var columnWidth = dom.parentNode.offsetWidth;
+      var collisionGroup = _findCollisions(dom);
 
       if (collisionGroup.length > 0) {
-        // TODO: this collision should create an offset to splittingWidth
-        // ex: var splittingWidth = columnWidth / (splitGroup.length + 1) - offset;
-        // offset => increment x for each collisionGroup
+        _resolveCollisions(dom, collisionGroup);
+      }
+    }
 
-        console.debug('Resolving collision: OLDEST_ON_LEFT');
+    function _findCollisions (dom) {
+      var stack = []
+        , collisions = new Set()
+        , currentNode
+        , sibling;
+
+      stack.push(dom);
+
+      while(stack.length > 0) {
+        currentNode = stack.shift();
+
+        var cardId = parseInt(currentNode.getAttribute('data-id'), 10);
+        sibling = dom.parentElement.parentElement.querySelectorAll('.planner-card:not([data-id="' + cardId + '"])');
+
+        for (var i = 0; i < sibling.length; i++) {
+          if (_collisionsDetector(currentNode, sibling[i])) {
+            if (!collisions.in(sibling[i])) {
+              collisions.update([currentNode, sibling[i]]);
+              stack.push(sibling[i]);
+            }
+          }
+        }
       }
 
-      if (splitGroup.length > 0) {
+      return collisions.items();
+    }
+
+    function _resolveCollisions (dom, collisionGroup) {
+      var startingPosition = Utils.offset(dom.parentNode).left
+        , columnWidth = dom.parentNode.offsetWidth;
+
+      // TODO: this collision should create an offset to splittingWidth
+//      if (collisionGroup.length > 0) {
+//        // ex: var splittingWidth = columnWidth / (splitGroup.length) - offset;
+//        // offset => increment x for each collisionGroup
+//
+//        console.debug('Resolving collision: OLDEST_ON_LEFT');
+//      }
+
+      // TODO: this isn't the right behaviour because I'm missing resolution according to conflict rule
+      if (collisionGroup.length > 0) {
         console.debug('Resolving collision: SPLITTING');
-        console.debug(splitGroup);
+        console.debug(collisionGroup);
 
-        var splittingWidth = columnWidth / (splitGroup.length + 1);
-
-        dom.style.width = splittingWidth + 'px';
-        dom.style.left = startingPosition + 'px';
-
-        for (var i = 0; i < splitGroup.length; i++) {
-          splitGroup[i].style.width = splittingWidth + 'px';
-          splitGroup[i].style.left = startingPosition + (splittingWidth * (i + 1)) + 'px';
+        var splittingWidth = columnWidth / collisionGroup.length;
+        for (var i = 0; i < collisionGroup.length; i++) {
+          collisionGroup[i].style.width = splittingWidth + 'px';
+          collisionGroup[i].style.left = startingPosition + (splittingWidth * i) + 'px';
         }
       }
     }
 
-    function _precedenceRules (firstMin, secondMin) {
+    // Collisions and rules
+    // --------------------
+
+    function _collisionsDetector (firstDom, secondDom) {
+      var min = parseInt(firstDom.getAttribute('data-start'), 10)
+        , max = parseInt(firstDom.getAttribute('data-end'), 10)
+        , currentMin = parseInt(secondDom.getAttribute('data-start'), 10)
+        , currentMax = parseInt(secondDom.getAttribute('data-end'), 10);
+
+      if (min >= currentMin && min <= currentMax || max >= currentMin && max <= currentMax) {
+        return true;
+      } else {
+        return currentMin >= min && currentMax <= max || currentMin >= min && currentMax <= max;
+      }
+    }
+
+    function _precedenceRules (firstDom, secondDom) {
+      var firstMin = parseInt(firstDom.getAttribute('data-start'), 10)
+        , secondMin = parseInt(secondDom.getAttribute('data-start'), 10);
+
       // Check if two elements have the same time (or time + 1)
       if (firstMin === secondMin || firstMin === secondMin + 1 || secondMin === firstMin + 1) {
         return SPLITTING;
@@ -80,7 +98,8 @@
       }
     }
 
-    planner.events.subscribe('cardDomDrawn', _detectCollisions);
+    // TODO: use this event AND a generic "drawingDone" otherwise there are too many (useless) conflict management
+    planner.events.subscribe('cardDomDrawn', _manager);
   };
 
   Collision.DEFAULTS = {};
@@ -90,4 +109,4 @@
 
   Plugins.register('collision', Collision);
 
-})(Planner.Plugins, Planner.Utils);
+})(Planner.Plugins, Planner.Utils, Ds.Set);
