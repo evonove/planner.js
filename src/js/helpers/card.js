68da714553cd74e8cf53d86b3b94456fc3e61908
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-08-28 22:25:39 +0200

    refactoring card draw and undraw with a more simple algorithm

--- src/js/helpers/card.js
+++ src/js/helpers/card.js
@@ -1,4 +1,4 @@
-(function (Instance, Utils, undefined) {
+(function (Instance, Utils, HashMap, undefined) {
   'use strict';
 
   var Mixin = {};
@@ -7,29 +7,27 @@
   // -----------------------
 
   Mixin.drawCard = function (card) {
-    var _cardDom;
-    var _cachedDomList = this.mapCard.get(card);
+    var _cardDom
+      , _cachedDomList = this.mapCard.get(card);
 
-    // Object assignment to a global hash map if it has been never initialized
-    if (typeof _cachedDomList === 'undefined') {
-      _cachedDomList = {};
-      this.mapCard.put(card, _cachedDomList);
+    if (typeof _cachedDomList !== 'undefined') {
+      // If this card has been already drawn, do nothing
+      return
     }
+
+    _cachedDomList = {};
+    this.mapCard.put(card, _cachedDomList);
 
     for (var i = 0; i < card.columns.length; i++) {
       var column = card.columns[i];
 
-      // Check if DOM was already drawn in the past otherwise return old instance
-      _cardDom = _cachedDomList[column];
-      if (typeof _cardDom === 'undefined') {
-        _cardDom = Utils.createElement(Planner.Templates.card(card));
-        this.updateDom(_cardDom, card, column);
+      _cardDom = Utils.createElement(Planner.Templates.card(card));
+      this.updateDom(_cardDom, card, column);
 
-        // Bidirectional mapping between Card and DOM object using a key/value
-        // literal to store all DOM objects
-        this.mapDom.put(_cardDom, card);
-        _cachedDomList[column] = _cardDom;
-      }
+      // Bidirectional mapping between Card and DOM object using a key/value
+      // literal to store all DOM objects
+      this.mapDom.put(_cardDom, card);
+      _cachedDomList[column] = _cardDom;
 
       this._drawDom(_cardDom);
     }
@@ -39,7 +37,6 @@
 
   Mixin._drawDom = function (cardDom) {
     // Get data-attributes element from card DOM
-    // TODO: provide an utility for a better data attribute access
     var dataColumn = parseInt(cardDom.getAttribute('data-column'), 10);
     var dataStart = parseInt(cardDom.getAttribute('data-start'), 10);
 
@@ -79,64 +76,57 @@
     }
   };
 
+  Mixin._removeDom = function(dom) {
+    dom.parentNode.removeChild(dom);
+
+    // Removing all bidirectional mapping
+    var card = this.mapDom.get(dom);
+    var domList = this.mapCard.get(card);
+
+    delete domList[parseInt(dom.getAttribute('data-column'), 10)];
+    this.mapDom.remove(dom);
+
+    this.events.publish('cardDomDeleted', [card, dom]);
+  };
+
+  Mixin.undrawCard = function(card) {
+    var domList = this.mapCard.get(card)
+      , dom;
+
+    for (var key in domList) {
+      if (domList.hasOwnProperty(key)) {
+        dom = domList[key];
+        this._removeDom(dom);
+      }
+    }
+
+    // Removing all mappings
+    this.mapCard.remove(card);
+    this.events.publish('cardDeleted', [this]);
+  };
+
+  Mixin.clear = function() {
+    // TODO: warning
+    // this is a shortcut to remove all cards without too much computation;
+    // this method isn't under MEM profiling and it's possible that it can
+    // cause useless memory waste
+
+    var domElement = document.querySelectorAll('.planner-card')
+      , dom;
+
+    for (var i = 0; i < domElement.length; i++) {
+      dom = domElement[i];
+      dom.parentNode.removeChild(dom);
+    }
+
+    // Logic delete waiting for the garbage collector to do the rest
+    this.mapCard = new HashMap();
+    this.mapDom = new HashMap();
+  };
+
   // Mixin for Instance
   // ------------------
 
   Instance.prototype = Utils.extend(Instance.prototype, Mixin);
 
-//    TODO: disabled for now
-//    Helpers.removeDom = function(cachedDomList, cardId) {
-//         var cardDom = cachedDomList[cardId];
-//
-//         _undrawDom.apply(this, [cardDom]);
-//
-//         // Removing all bidirectional mapping
-//         delete cachedDomList[cardId];
-//         Planner.mapDom.remove(cardDom);
-//         Planner.Events.publish('cardDomDeleted', [this, cardDom]);
-//    };
-
-//    TODO: disabled for now
-//    Helpers.undrawDom = function(cardDom) {
-//        // Remove Card column representation from the DOM
-//        cardDom.remove();
-//        Planner.Events.publish('cardDomUndrawn', [this, cardDom]);
-//    };
-
-//    TODO: disabled for now
-//    Helpers.clearDomCache = function(cachedDomList) {
-//        // Clear all cached objects
-//        for (var cardId in cachedDomList) {
-//            if (cachedDomList.hasOwnProperty(cardId)) {
-//                _removeDom.apply(this, [cachedDomList, cardId]);
-//            }
-//        }
-//    };
-
-//    TODO: disabled for now
-//    Planner.Instance.prototype.undrawCard = function(card) {
-//        var _cachedDomList = Planner.mapCard.get(this);
-//        var _cardDom;
-//
-//        // Undraw all related DOM objects
-//        for (var cardId in _cachedDomList) {
-//            if (_cachedDomList.hasOwnProperty(cardId)) {
-//                _cardDom = _cachedDomList[cardId];
-//                _undrawDom.apply(this, [_cardDom]);
-//            }
-//        }
-//
-//        Planner.Events.publish('cardUndrawn', [this]);
-//    };
-
-//    TODO: disabled for now
-//    Planner.Instance.prototype.removeCard = function(card) {
-//        var _cachedDomList = Planner.mapCard.get(this);
-//
-//        // Removing all mappings
-//        _clearDomCache.apply(this, [_cachedDomList]);
-//        Planner.mapCard.remove(this);
-//        Planner.Events.publish('cardDeleted', [this]);
-//    };
-
-})(Planner.Instance = Planner.Instance || {}, Planner.Utils);
+})(Planner.Instance = Planner.Instance || {}, Planner.Utils, Ds.HashMap);
