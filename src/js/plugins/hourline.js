04034752ba9245ee2219da4771c2e1b7348aab71
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-12-10 10:00:07 +0100

    hourline refactoring

--- src/js/plugins/hourline.js
+++ src/js/plugins/hourline.js
@@ -1,40 +1,50 @@
 (function (window, Plugins, Utils, undefined) {
   'use strict';
 
-  // Plugin constructor and defaults
-  // -------------------------------
+  // Plugin constructor
+  // ------------------
 
-  var Hourline = function (element, options) {
+  function Hourline (element, options) {
     var hourline = Utils.createElement(Planner.Templates.hourline());
 
-    element.appendChild(hourline);
+    element.querySelector('.planner-container').appendChild(hourline);
 
     scrollScreen();
     moveLine();
 
     window.setInterval(moveLine, options.hourlineTickUpdate * 3600);
 
+    // Public methods
+    // --------------
+
+    this.findHeight = findHeight;
+
     // Helpers
     // -------
 
-    function findHeight (date) {
-      var effectiveHours = date.getHours() + (date.getMinutes() / 60);
+    function findHeight () {
+      var date = new Date(),
+          effectiveHours = date.getHours() + (date.getMinutes() / 60);
+
       return (effectiveHours * options.timeslotHeight * options.timeslots) + options.hourlineMargin;
     }
 
     function scrollScreen () {
-      var hourlineTop = findHeight(new Date())
-        , offset = window.screen.height / options.hourlineScreenSection;
+      var hourlineTop = findHeight(),
+          offset = window.screen.height / options.hourlineScreenSection;
 
       window.scroll(0, hourlineTop - offset);
     }
 
     function moveLine () {
-      var hourlineTop = findHeight(new Date());
+      var hourlineTop = findHeight();
 
       hourline.style.top = hourlineTop + 'px';
     }
-  };
+  }
+
+  // Defaults
+  // --------
 
   Hourline.DEFAULTS = {
     hourlineMargin: 85,
@@ -44,7 +54,7 @@
 
   // Register this plugin to plugins list
   // ------------------------------------
-
+  Plugins.Hourline = Hourline;
   Plugins.register('hourline', Hourline);
 
 })(window, Planner.Plugins, Planner.Utils);
