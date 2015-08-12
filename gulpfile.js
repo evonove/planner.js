741bfdc6387de90890c094446738c19c7586f5e7
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-08-12 14:24:02 +0200

    wheelie integration for Handlebars and JavaScript build processes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,52 @@ var gulp = require('gulp');
 var wheelie = require('wheelie');
 var recipe = require('wheelie-recipe');
 
+
 wheelie.add(recipe);
 wheelie.setDefault('watch');
+
+// appending JavaScript files
+var vendors = [
+  'handlebars/handlebars.runtime.js',
+  'ds.js/dist/hashmap.js',
+  'ds.js/dist/set.js',
+  'messengerjs/dist/messenger.js'
+];
+
+var scripts = [
+  'models/models.js',
+  'planner.js',
+  'compat/modernizr.js',
+  'instance.js',
+  'utils.js',
+  'helpers/generic.js',
+  'helpers/instance.js',
+  'helpers/card.js',
+  'models/card.js',
+  'templates.js',
+  'plugins/manager.js',
+  'plugins/slider.js',
+  'plugins/collision.js',
+  'plugins/mobile.js',
+  'plugins/hourline.js',
+  'plugins/interaction/interaction.js',
+  'plugins/interaction/actions/create.js',
+  'plugins/interaction/actions/drag.js',
+  'plugins/interaction/actions/interaction.js',
+  'plugins/interaction/actions/resize.js',
+  'plugins/interaction/handlers/mouse.js',
+  'plugins/interaction/handlers/drag.js',
+  'plugins/interaction/handlers/touch.js',
+  'main.js'
+];
+
+// tasks configurations
+wheelie.registry.get('uglify').dependencies = ['handlebars'];
+wheelie.update('handlebars', { namespace: 'Planner.Templates' });
+wheelie.update('uglify', {
+  scripts: scripts,
+  vendors: vendors,
+  outputName: 'planner.js'
+});
+
 wheelie.build();
